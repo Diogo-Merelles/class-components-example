9af435b74b7815473c71be7fff347e24a7e0ace4
Diogo Merelles <diogo.merelles@nb-dmerelles.local>
2022-11-04 10:47:02 +0000

    finished excercice

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Register from './Containers/Register';
+
+function App() {
+  return (
+      <div className="App">
+
+      <BrowserRouter>
+      
+        <Routes>
+            <Route path="/" element={<Register title={"New Work"}/>}/>
+
+        </Routes>
+      
+      </BrowserRouter>
+      
+    </div>
+
+  );
+}
+
+export default App;
